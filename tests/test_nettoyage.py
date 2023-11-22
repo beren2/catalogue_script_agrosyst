69a38e58bdf2ddc3f8fe73c294ac60c3d079493e
Bérenger <berenger.vuittenez@inrae.fr>
2023-11-22 09:10:57 +0100

    utilisation de get_info_all dans le test get_dose_ref

--- tests/test_nettoyage.py
+++ tests/test_nettoyage.py
@@ -120,103 +120,16 @@ def test_get_dose_ref():
     # obtention des données
     path_data = 'tests/data/test_get_dose_ref/'
     path_utilisation_intrant_realise = path_data+'utilisation_intrant_realise.csv'
-    path_intrant = path_data+'intrant.csv'
-    path_intervention_realise = path_data+'intervention_realise.csv'
-    path_plantation_perenne_realise = path_data+'plantation_perenne_realise.csv'
-    path_plantation_perenne_phase_realise = path_data+'plantation_perenne_phase_realise.csv'
-    path_composant_culture = path_data+'composant_culture.csv'
-    path_noeuds_realise = path_data+'noeuds_realise.csv'
-    path_utilisation_intrant_cible = path_data+'utilisation_intrant_cible.csv'
-    path_intervention_synthetise = path_data+'intervention_synthetise.csv'
-    path_plantation_perenne_phase_synthetise = path_data+'plantation_perenne_phase_synthetise.csv'
-    path_plantation_perenne_synthetise = path_data+'plantation_perenne_synthetise.csv'
-    path_noeuds_synthetise = path_data+'noeuds_synthetise.csv'
     path_utilisation_intrant_synthetise = path_data+'utilisation_intrant_synthetise.csv'
-    path_connection_synthetise = path_data+'connection_synthetise.csv'
-    path_culture = path_data+'culture.csv'
 
     df_utilisation_intrant_realise = pd.read_csv(path_utilisation_intrant_realise, sep = ',') 
-    df_intrant = pd.read_csv(path_intrant, sep = ',') 
-    df_intervention_realise = pd.read_csv(path_intervention_realise, sep = ',') 
-    df_plantation_perenne_realise = pd.read_csv(path_plantation_perenne_realise, sep =',') 
-    df_plantation_perenne_phase_realise = pd.read_csv(path_plantation_perenne_phase_realise, sep =',') 
-    df_composant_culture = pd.read_csv(path_composant_culture, sep = ',')
-    df_noeuds_realise = pd.read_csv(path_noeuds_realise, sep = ',')
-    df_utilisation_intrant_cible = pd.read_csv(path_utilisation_intrant_cible, sep = ',')
-    df_intervention_synthetise = pd.read_csv(path_intervention_synthetise, sep = ',')
-    df_plantation_perenne_phase_synthetise = pd.read_csv(path_plantation_perenne_phase_synthetise, sep = ',')
-    df_plantation_perenne_synthetise = pd.read_csv(path_plantation_perenne_synthetise, sep = ',')
-    df_culture = pd.read_csv(path_culture, sep =',')
-    df_noeuds_synthetise = pd.read_csv(path_noeuds_synthetise, sep = ',')
     df_utilisation_intrant_synthetise = pd.read_csv(path_utilisation_intrant_synthetise, sep = ',')
-    df_connection_synthetise = pd.read_csv(path_connection_synthetise, sep = ',')
 
-    # test de l'affectation des informations du traitement
-    test_get_infos_traitement_realise = fonctions_utiles.get_infos_traitement(df_utilisation_intrant_realise[['id', 'intrant_id']], df_intrant)
-    test_get_infos_traitement_synthetise = fonctions_utiles.get_infos_traitement(df_utilisation_intrant_synthetise[['id', 'intrant_id']], df_intrant)
-    test_get_infos_traitement_ = pd.concat([test_get_infos_traitement_realise, test_get_infos_traitement_synthetise], axis=0)
-
-    # test de l'affectation des informations de la culture
-    test_get_infos_culture_realise = fonctions_utiles.get_infos_culture_realise(
-        df_utilisation_intrant_realise, 
-        df_intervention_realise, 
-        df_noeuds_realise,
-        df_plantation_perenne_realise,
-        df_plantation_perenne_phase_realise,
-        df_composant_culture,
-    )
-    test_get_infos_culture_synthetise = fonctions_utiles.get_infos_culture_synthetise(
-        df_utilisation_intrant_synthetise, 
-        df_intervention_synthetise, 
-        df_connection_synthetise,
-        df_noeuds_synthetise,
-        df_plantation_perenne_synthetise,
-        df_plantation_perenne_phase_synthetise,
-        df_composant_culture,
-        df_culture
-    )
-    test_get_infos_culture = pd.concat([test_get_infos_culture_realise, test_get_infos_culture_synthetise], axis=0)
-
-    # test de l'affectation des informations de la cible
-    test_get_infos_cible = fonctions_utiles.get_infos_cible(
-        df_utilisation_intrant_cible
-    )
-
-    df_utilisation_intrant = pd.concat([df_utilisation_intrant_realise, df_utilisation_intrant_synthetise], axis=0)
-
-    # Obtention des informations des traitements
-    left = df_utilisation_intrant
-    right = test_get_infos_traitement_[[
-        'id',
-        'id_produit', 
-        'id_traitement', 
-        'code_amm', 
-        'code_traitement_maa'
-    ]]
-    total_merge_1 = pd.merge(left, right, on = 'id', how='left')
-
-    # Obtention des informations des cultures
-    left = total_merge_1 
-    right = test_get_infos_culture[[
-        'id', 
-        'code_culture_maa'
-    ]]
-    total_merge_2 = pd.merge(left, right, on = 'id', how='left')
-
-    # Obtention des informations sur les cibles
-    left = total_merge_2
-    right = test_get_infos_cible[[
-        'id', 
-        'code_groupe_cible_maa'
-    ]]
-    total_merge_3 = pd.merge(left, right, on = 'id', how='left')
-
-    # On obtient une ligne dont la clé primaire est : [['id', 'code_amm', 'code_culture_maa', 'code_traitement_maa', 'code_groupe_cible_maa']]
-
-    # test de l'obtention de la dose de référence
-    test_get_dose_ref_ = fonctions_utiles.get_dose_ref(
-        total_merge_3
-    )
+    # obtention de la dose de référence à partir de la fonction get_infos_all_utilisation_intrant
+    df_utilisation_intrant_realise = fonctions_utiles.get_infos_all_utilisation_intrant(df_utilisation_intrant_realise, saisie = 'realise', path_data=path_data)
+    df_utilisation_intrant_synthetise = fonctions_utiles.get_infos_all_utilisation_intrant(df_utilisation_intrant_synthetise, saisie = 'synthetise', path_data=path_data)
+    
+    test_get_dose_ref_ = pd.concat([df_utilisation_intrant_realise, df_utilisation_intrant_synthetise], axis=0)
 
     # obtention de la dose de référence attendue
     dose_ref_expected = df_metadonnees.loc[df_metadonnees['colonne_testee'] == 'dose_ref_maa'][['id_ligne', 'valeur_attendue']]
