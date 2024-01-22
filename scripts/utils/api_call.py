160a20880e0b6dfe41b50bcf9da7c04a2b071bc8
Bérenger <berenger.vuittenez@inrae.fr>
2024-01-22 16:45:50 +0100

    Acorrections typo

--- scripts/utils/api_call.py
+++ scripts/utils/api_call.py
@@ -2,14 +2,14 @@
     Ce script contient les fonctions utiles pour l'appel à l'api Datagrosyst : 
     (obtention des métadonnées de l'entrepôt, mise à jour des données...)
 """
-import requests
-from requests.auth import HTTPBasicAuth
-from io import StringIO
-import pandas as pd
 import json
 from datetime import datetime, date
+from io import StringIO
 import os
 import configparser
+from requests.auth import HTTPBasicAuth
+import requests
+import pandas as pd
 
 
 # Obtention des données de configurations pour l'API et la mise à jour des données
@@ -24,34 +24,32 @@ LAST_UPDATE_DATE_FILE = config.get('api_info', 'last_update_date_file')
 DATAGROSYST_MAIL = config.get('api_info', 'mail')
 DATAGROSYST_PASSWORD = config.get('api_info', 'password')
 BASIC = HTTPBasicAuth(DATAGROSYST_MAIL, DATAGROSYST_PASSWORD)
+TIMEOUT = 100
 
 def get_metadata(metadata_name):
     """ 
         Chargement de la dernière date d'actualisation des exports personnalisés
     """
-    global URL
     end_point = URL+'metadata/'+metadata_name
-    r = requests.get(end_point)  
+    r = requests.get(end_point, timeout=TIMEOUT)  
     return json.loads(r.content)
 
 def get_table_names():
     """
         Obtenir le nom de toutes les tables de l'entrepôt de données
     """
-    global URL
     end_point = URL+'table_name/'
-    r = requests.get(end_point)  
+    r = requests.get(end_point, timeout=TIMEOUT)  
     return json.loads(r.content)
 
 def download_df(df_name, verbose=False):
     """ 
         Chargement d'une table dans un dataframe par appel à l'api
     """
-    global URL
     if(verbose):
         print("- "+df_name)
     end_point = URL+'table_zip/'+df_name
-    r = requests.get(end_point, auth=BASIC)  
+    r = requests.get(end_point, auth=BASIC, timeout=TIMEOUT)  
     return pd.read_csv(StringIO(r.text), sep=',')
 
 def download_dfs(df_names, data_path_out='./data/', verbose=False):
@@ -61,12 +59,13 @@ def download_dfs(df_names, data_path_out='./data/', verbose=False):
     for df_name in df_names :
         df = download_df(df_name, verbose=verbose)
         df.to_csv(data_path_out+df_name+".csv")
-    return 
 
 def load_df(df_name, data_path_in, verbose=False):
     """ 
         Chargement d'une table locale dans un dataframe 
     """
+    if(verbose):
+        print("- "+df_name)
     return pd.read_csv(data_path_in+df_name+'.csv', sep=',', index_col=0)
 
 def loads_dfs(df_names, data_path_in='./data/', verbose=False):
@@ -88,8 +87,8 @@ def refresh_last_data(data_path):
     need_refresh = True
     if os.path.isfile(LAST_UPDATE_DATE_FILE):
         # le fichier de dernier import existe
-        file = open(LAST_UPDATE_DATE_FILE)
-        last_import_date = datetime.strptime(file.read().splitlines()[0], "%Y-%m-%d")
+        with open(LAST_UPDATE_DATE_FILE, "r", encoding="utf8") as file:
+            last_import_date = datetime.strptime(file.read().splitlines()[0], "%Y-%m-%d")
     
         if(last_import_date >= current_export_date):
             # il y a eu une nouvelle version d'exports depuis la dernière mise à jour des données locales
@@ -101,14 +100,11 @@ def refresh_last_data(data_path):
         # téléchargement des données
         print("Téléchargement [peut prendre plusieurs dizaines de minutes]...")
         df_names = get_table_names()
-        dfs = download_dfs(df_names, data_path_out=data_path, verbose=True)
+        download_dfs(df_names, data_path_out=data_path, verbose=True)
         print("Téléchargement terminé !")
 
         # mise à jour des données locales : aujourd'hui
-        file = open(LAST_UPDATE_DATE_FILE, 'w+')
-        file.write(str(date.today()))
-        file.close()
+        with open(LAST_UPDATE_DATE_FILE, "w+", encoding="utf8") as file:
+            file.write(str(date.today()))
     else :
         print("Vos données sont à jour !")
-
-    return 
