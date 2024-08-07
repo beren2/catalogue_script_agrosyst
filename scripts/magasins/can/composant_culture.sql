38256e2574985365e141cf71a9205c5b981e906b
Bérenger <berenger.vuittenez@inrae.fr>
2024-08-07 15:50:31 +0200

    ajout des premiers scripts sql pour téléchargement du magasin CAN

---
+++ scripts/magasins/can/composant_culture.sql
@@ -0,0 +1,35 @@
+select 
+	ed.code as domaine_code, 
+	ed.id as domaine_id,
+	ed.nom as domaine_nom, 
+	ed.campagne as domaine_campagne, 
+	ec.code as culture_code, 
+	ec.id as culture_id, 
+	ec.nom as culture_nom, 
+	CASE ec.type
+      	WHEN 'MAIN' then 'PRINCIPALE'
+      	WHEN 'CATCH' then 'DEROBEE'
+      	WHEN 'INTERMEDIATE' then 'INTERMEDIAIRE' end culture_type,
+	case ec.melange_especes
+		when true then 'oui'
+		when false then 'non' end culture_melange_especes,
+	case ec.melange_varietes
+		when true then 'oui'
+		when false then 'non' end culture_melange_varietes,
+	ecc.id as espece_id, 
+	ecc.code as espece_code, 
+	ecc.surface_relative,
+	ee.code_espece_botanique,
+	ee.code_qualifiant_aee as code_edi_qualifiant,
+	ee.code_type_saisonnier_aee as code_edi_type_saisonnier,
+	ee.code_destination_aee as code_edi_destination,
+	ee.libelle_espece_botanique as espece_edi_nom_botanique,
+	ee.libelle_qualifiant_aee as espece_edi_qualifiant,
+	ee.libelle_type_saisonnier_aee as espece_edi_type_saisonnier,
+	ee.libelle_destination_aee as espece_edi_destination,
+	ev.nom
+from entrepot_composant_culture ecc
+inner join entrepot_culture ec on ecc.culture_id  = ec.id
+left join entrepot_espece ee on ecc.espece_id = ee.id 
+left join entrepot_variete ev on ecc.variete_id = ev.id
+left join entrepot_domaine ed on ec.domaine_id = ed.id;
---
+++ scripts/magasins/can/dispositif.sql
@@ -0,0 +1,11 @@
+select 
+	d.code as domaine_code, 
+	d.id as domaine_id, 
+	d.nom as domaine_nom, 
+	d.campagne as domaine_campagne, 
+	ed.id as dispositif_id, 
+	ed.code as dispositif_code,
+	ed.nom as dispositif_nom, 
+	ed.type as dispositif_type
+from entrepot_dispositif ed
+left join entrepot_domaine d on d.id = ed.domaine_id;
