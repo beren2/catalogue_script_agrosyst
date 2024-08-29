051e0f9c04adeaa7bbd7ae1929073670edc21647
Bérenger <berenger.vuittenez@inrae.fr>
2024-08-29 11:30:34 +0200

    ajout atelier elevage, coordonnées gps

---
+++ scripts/magasins/can/atelier_elevage.sql
@@ -0,0 +1,9 @@
+select 
+	ed.id as domaine_id,
+	eae.id as atelier_elevage_id, 
+	eae.type_animaux as atelier_elevage_type_animaux,
+	eae.taille_elevage as atelier_elevage_taille,
+	eae.taille_elevage_unite as atelier_elevage_unite
+from entrepot_atelier_elevage eae
+left join entrepot_domaine ed on ed.id = eae.domaine_id
+join domaine_filtre df on ed.id = df.domaine_id;
---
+++ scripts/magasins/can/coordonees_gps_domaine.sql
@@ -0,0 +1,8 @@
+select 
+	ecgd.domaine_id,
+	ecgd.nom_coordonnees_centre_operationnel as coordonnees_nom_centre_operationnel,
+	ecgd.coordonnees_description_centre_operationnel,
+	ecgd.longitude,
+	ecgd.latitude
+from entrepot_coordonees_gps_domaine ecgd
+join domaine_filtre df on df.domaine_id = ecgd.domaine_id;
