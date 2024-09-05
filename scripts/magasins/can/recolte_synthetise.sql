85f4e658faf6fcb4e1cdff41d4b65d3ab4c42e68
Bérenger <berenger.vuittenez@inrae.fr>
2024-09-05 16:40:08 +0200

    recolte_synthetise update

--- scripts/magasins/can/recolte_synthetise.sql
+++ scripts/magasins/can/recolte_synthetise.sql
@@ -1,28 +1,3 @@
--- On génère d'abord la table agrégée complète
-CREATE TEMPORARY TABLE entrepot_action_synthetise_agrege AS
-SELECT DISTINCT 
-	nuirac.action_synthetise_id as id,
-    nuirac.intervention_synthetise_id, 
-    nuirac.plantation_perenne_synthetise_id, 
-    nuirac.connection_synthetise_id, 
-    nuirac.cible_noeuds_synthetise_id,
-    nuirac.plantation_perenne_phases_synthetise_id, 
-    nuirac.synthetise_id, 
-    nuirac.sdc_campagne, 
-    nuirac.sdc_id, 
-    nuirac.domaine_id, 
-    nuirac.dispositif_id
-FROM
-    entrepot_action_synthetise air 
-LEFT JOIN "entrepot_utilisation_intrant_synthetise_agrege" nuirac on air.id = nuirac.action_synthetise_id
-UNION 
-SELECT 
-    id, 
-    intervention_synthetise_id,
-    plantation_perenne_synthetise_id, connection_synthetise_id, cible_noeuds_synthetise_id, plantation_perenne_phases_synthetise_id, synthetise_id, 
-    sdc_campagne, sdc_id, domaine_id, dispositif_id
-FROM entrepot_action_synthetise_manquant_agrege earma;
-
 select 
 	ed.code as domaine_code,
 	earma.domaine_id,
