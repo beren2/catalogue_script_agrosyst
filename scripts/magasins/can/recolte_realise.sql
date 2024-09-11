4844618a678afdece24bff1a1b07f3aa2ee0cd85
Bérenger <berenger.vuittenez@inrae.fr>
2024-09-11 14:38:16 +0200

    prise en compte des filtres + correction synthétisé

--- scripts/magasins/can/recolte_realise.sql
+++ scripts/magasins/can/recolte_realise.sql
@@ -7,13 +7,13 @@ select
 	esdc.nom as sdc_name,
 	earma.parcelle_id,
 	ep.nom as parcelle_nom,
-	earma.zone_id, 
+	earma.zone_id,
 	ez.nom as zone_nom,
 	earma.intervention_realise_id, 
 	errp.action_id,
 	ear.label as type_action,
-	errp.destination_id, 
-	errp.destination_id as destination_nom,  --attention, remplacer par le destination_nom à trouver dans le référentiel éponyme
+	errp.destination_id,
+	dv.libelle as destination_nom, 
 	errp.rendement_moy_corr as rendement_moyen,
 	errp.rendement_median_corr as rendement_median,
 	errp.rendement_min_corr as rendement_min,
@@ -29,6 +29,5 @@ left join entrepot_domaine ed on earma.domaine_id = ed.id
 left join entrepot_sdc esdc on earma.sdc_id = esdc.id
 left join entrepot_zone ez on earma.zone_id = ez.id
 left join entrepot_parcelle ep on earma.parcelle_id = ep.id
+left join entrepot_destination_valorisation dv on errp.destination_id = dv.id
 join entrepot_dispositif_filtres_outils_can edfoc on esdc.dispositif_id = edfoc.id;
-
-
