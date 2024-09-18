1cb4ad6550d5279c4e98cf4bef192f2bed986215
Lucie Baude <lucie.baude@inrae.fr>
2024-09-18 08:53:40 +0200

    Ajout de colonnes

--- scripts/magasins/can/intervention_synthetise.sql
+++ scripts/magasins/can/intervention_synthetise.sql
@@ -4,17 +4,19 @@ SELECT
     d.code as domaine_code,
     d.id as domaine_id,
     d.nom as domaine_nom,
+    d.code as domaine_code,
     d.campagne as domaine_campagne,
     sdc.code as sdc_code,
     sdc.id as sdc_id,
     sdc.nom as sdc_nom,
-    s.id as synthetise_id,
-    s.nom as synthetise_nom,
+    s.id as systeme_synthetise_id,
+    s.nom as systeme_synthetise_nom,
+    s.campagnes as systeme_synthetise_campagnes,
+    s.valide as systeme_synthetise_validation,
     pppr.id as phase_id, 
     pppr.type as phase,
-    nr.culture_code as culture_code, 
+    c.code as culture_code, 
     (replace(replace(c.nom,CHR(13)||CHR(10),'<br>'), CHR(10), '<br>')) as culture_nom,
-    c_i.id as ci_id,
     c_i.code as ci_code,
     (replace(replace(c_i.nom,CHR(13)||CHR(10),'<br>'), CHR(10), '<br>')) as ci_nom,
     CASE ir.concerne_ci WHEN true THEN 'oui' WHEN false THEN 'non' END concerne_la_ci,
@@ -22,8 +24,10 @@ SELECT
 	iroc.precedent_code,
     (replace(replace(iroc.precedent_nom,CHR(13)||CHR(10),'<br>'), CHR(10), '<br>')) as precedent_nom,
 	iroc.precedent_especes_edi,
+    ecs.id as culture_precedent_rang_id,
     nr.rang+1 as culture_rang,
     ir.id as intervention_id,
+    ir.rang as rang_intervention,
     ir.type as intervention_type,
     iroc.interventions_actions,
     ir.nom as intervention_nom,
@@ -55,6 +59,7 @@ FROM  entrepot_intervention_synthetise ir
 LEFT JOIN entrepot_intervention_synthetise_agrege_extanded irae ON ir.id = irae.id
 LEFT JOIN entrepot_intervention_synthetise_outils_can iroc ON irae.id = iroc.intervention_synthetise_id
 LEFT JOIN entrepot_noeuds_synthetise nr ON irae.cible_noeuds_synthetise_id = nr.id
+LEFT JOIN entrepot_connection_synthetise ecs ON ecs.source_noeuds_synthetise_id = nr.id
 LEFT JOIN entrepot_plantation_perenne_phases_synthetise pppr ON irae.plantation_perenne_phases_synthetise_id = pppr.id
 LEFT JOIN entrepot_plantation_perenne_realise eppr on pppr.plantation_perenne_synthetise_id = eppr.id 
 LEFT JOIN entrepot_synthetise s ON nr.synthetise_id = s.id
