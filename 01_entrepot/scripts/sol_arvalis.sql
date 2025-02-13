fbb91f7ad7f58f7483b0b90f2b7dc2fca5b953b4
Bérenger <berenger.vuittenez@inrae.fr>
2025-02-13 09:43:53 +0100

    ajout de la génération du ref sol arvalis

---
+++ 01_entrepot/scripts/sol_arvalis.sql
@@ -0,0 +1,24 @@
+CREATE TABLE entrepot_sol_arvalis AS
+	select 
+		r.topiaid as id, 
+		r.id_type_sol as type_sol, 
+		r.sol_nom as nom,
+		r.sol_calcaire as calcaire,
+		r.sol_hydromorphie as hydromorphie, 
+		r.sol_pierrosite as pierrosite, 
+		r.sol_profondeur as profondeur, 
+		r.sol_texture as texture, 
+		r.sol_region as region, 
+		r.sol_region_code as region_code,
+		r.source as source,
+		r.sol_calcaire_typecode as calcaire_type_code, 
+		r.sol_hydromorphie_typecode as hydromorphie_type_code, 
+		r.sol_profondeur_typecode as profondeur_type_code, 
+		r.sol_texture_typecode as texture_type_code
+	from refsolarvalis r
+	where r.active is true;
+
+
+alter table entrepot_sol_arvalis
+add constraint sol_arvalis_PK
+PRIMARY KEY (id);
