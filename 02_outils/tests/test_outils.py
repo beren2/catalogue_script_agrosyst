1f517d93552263e2b44b04d92ed1ea8fa5d27482
Thomas Badie <thomas.badie@outlook.fr>
2025-04-11 17:17:51 +0200

    Ajout des TUs pour la fonction principale (avec ses 2 variables) dont prise en compte TUs de Bérenger + TUs de la fonction listant les synthétisés bien structurés

--- 02_outils/tests/test_outils.py
+++ 02_outils/tests/test_outils.py
@@ -655,7 +655,7 @@ def test_get_typologie_culture_CAN():
     identifiant_test = 'test_get_typologie_culture_CAN'
     df_names = [   
                     'composant_culture', 'culture', 
-                    'espece_vCAN', # A changer lorsque le refespece sera pret
+                    'espece',
                     'typo_especes_typo_culture','typo_especes_typo_culture_marai' # referentiel CAN
                 ]
     path_data = '02_outils/tests/data/test_get_typologie_culture_CAN/'
@@ -666,6 +666,38 @@ def test_get_typologie_culture_CAN():
     res = pd.Series(res).fillna(False).all()
 
     assert res
+
+
+def extract_good_rotation_diagram():
+    """
+        Test de l'obtention de la liste de "bon" synthétisé pour la suite (soit la fonction poids des rotations en synthétisé)
+        ATTENTION va prendre les même données d'entrée que la fonction test_get_connexion_weight_in_synth_rotation(. donc path_data ne change pas
+    """
+    df_names = [   
+                    'noeuds_synthetise', 'connection_synthetise'
+                ]
+    path_data = '02_outils/tests/data/test_get_connexion_weight_in_synth_rotation/'
+    donnees = import_dfs(df_names, path_data, {}, sep = ',')
+    res_to_test, _ = indicateur.extract_good_rotation_diagram(donnees)
+    
+    good_to_check = ['fr.inra.agrosyst.api.entities.practiced.PracticedSystem_d4a1b64c-afa0-440f-92e1-30a483871ab4',
+    'fr.inra.agrosyst.api.entities.practiced.PracticedSystem_00b5a0b4-39a6-4822-802b-e81fd44386a2',
+    'fr.inra.agrosyst.api.entities.practiced.PracticedSystem_05e5d9d6-ad2b-43b2-b37a-1f32850e37a1',
+    'fr.inra.agrosyst.api.entities.practiced.PracticedSystem_61f75804-3823-4fae-9ce1-82bfa3d7e41e',
+    'fr.inra.agrosyst.api.entities.practiced.PracticedSystem_41ae9d22-5515-44d8-9a7f-8254c42149eb',
+    'fr.inra.agrosyst.api.entities.practiced.PracticedSystem_2f68d851-cb30-402f-bfc6-b0abf37c49a8',
+    'fr.inra.agrosyst.api.entities.practiced.PracticedSystem_1e606337-4238-437b-9856-a302b2431efd',
+    'fr.inra.agrosyst.api.entities.practiced.PracticedSystem_91611752-8cd2-42f1-b19f-97186597ab64',
+    'fr.inra.agrosyst.api.entities.practiced.PracticedSystem_445dd407-58f6-403e-8dd6-8352166a0131',
+    'fr.inra.agrosyst.api.entities.practiced.PracticedSystem_0316e326-6369-4ec7-ab50-b983c3aa0b3d',
+    'fr.inra.agrosyst.api.entities.practiced.PracticedSystem_000f911f-5e67-4280-ae77-35098c17aa5d',
+    'fr.inra.agrosyst.api.entities.practiced.PracticedSystem_592b3792-8ad0-4213-ab98-a948f444ed04']
+
+    if set(list(res_to_test)) == set(good_to_check) : res = True
+    else : res = False
+
+    assert res
+
 
 
 def test_get_connexion_weight_in_synth_rotation():
