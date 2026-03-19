d151dfa404a6c649ac1f801023775262a1382548
Thomas Badie <thomas.badie@outlook.fr>
2026-03-19 18:01:34 +0100

    début TU pour le test de la création des etat_temporel pour Dirodur

---
+++ 02_outils/tests/test_outils_dirodur.py
@@ -0,0 +1,131 @@
+"""
+    Regroupe tous les tests utilisés pour vérifier que les outils "Dirodur" tournent correctement
+"""
+import geopandas as gpd
+import pandas as pd
+from scripts import dirodur_outil
+from scripts import dirodur_util
+
+
+def import_df(df_name, path_data, sep, df, file_format='csv'):
+    """
+        importe un dataframe au chemin path_data+df_name+'.csv' et le stock dans le dictionnaire 'df' à la clé df_name
+    """
+    if file_format == 'csv' :
+        df[df_name] = pd.read_csv(path_data+df_name+'.'+file_format, sep = sep, 
+                                  dtype = {'codeinsee':str,
+                                            'departement':str,
+                                            'codepostal':str,
+                                            'region':str,
+                                            'arrondissement_code':str,
+                                            'bassin_vie':str,
+                                            'zone_emploi':str,
+                                            
+                                            'cell':str})
+    if file_format == 'json' and df_name.startswith('geoVec') :
+        # Utilise geopandas pour les json formater en geojson. Le nom du fichier json doit alors commencer par geoVec
+        df[df_name] = gpd.read_file(path_data+df_name+'.'+file_format)
+    if file_format == 'gpkg' :
+        df[df_name] = gpd.read_file(path_data+df_name+'.'+file_format)
+
+def import_dfs(df_names, data_path, sep = ',', df=None, file_format='csv'):
+    """
+        stocke dans le dictionnaire df tous les dataframes indiqués dans la liste df_names
+    """
+    if df is None:
+        df = {}
+    for df_name in df_names : 
+        import_df(df_name, path_data=data_path, df = df, sep = sep, file_format=file_format)
+
+    return df
+
+def import_dfs_withExtension(df_names_withExt:dict, data_path):
+    """
+        stocke dans le dictionnaire df tous les dataframes indiqués dans le dictionnaire df_names_withExt qui prend en key l'extension des fichiers, lié à une liste de nom de fichiers
+    """
+    all_df = {}
+    for x in df_names_withExt :
+        if isinstance(x, str) and x in {'json', 'gpkg', 'csv'}:
+            df_names = df_names_withExt[x]
+            df_dict = import_dfs(df_names, data_path, file_format=x)
+            all_df = {**all_df, **df_dict}
+        else :
+            raise Exception("Les clefs du dictionnaire doivent être 'csv' ou 'json' ou 'gpkg'") 
+    return all_df
+
+def fonction_test(identifiant_test, df_names, path_data, fonction_to_apply, metadonnee_file='02_outils/tests/metadonnees_tests_unitaires.csv', df_ref_names = None, path_ref = '02_outils/data/referentiels/', key_name='id', multi_extension:bool = False):
+    """
+        Fonction qui permet de tester 
+    """
+    df_metadonnees = pd.read_csv(metadonnee_file)
+    df_metadonnees = df_metadonnees.loc[df_metadonnees['identifiant_test'] == identifiant_test]
+
+    # dictionnaire donnant pour chaque identifiant d'entité (par exemple intervention_id), les colonnes à tester
+    colonne_to_test_for_ligne = df_metadonnees.groupby('id_ligne').agg({'colonne_testee' : ','.join}).to_dict()['colonne_testee']
+    for (key, value) in colonne_to_test_for_ligne.items():
+        colonne_to_test_for_ligne[key] = value.split(',')
+
+    if multi_extension :
+        donnees_ = import_dfs_withExtension(df_names, data_path = path_data)
+    else :
+        donnees_ = import_dfs(df_names, path_data, df = {}, sep = ',')
+    donnees_ref = {}
+    if(not df_ref_names is None):
+        # dans le cas où on a des données sensibles, celles-ci sont encryptées et importées
+        donnees_ref = import_dfs(df_ref_names, path_ref, df = {}, sep = ',')
+    donnees = donnees_ | donnees_ref
+    
+    donnees_computed = fonction_to_apply(donnees)
+    #donnees_computed.to_csv('~/Bureau/Datagrosyst/catalogue_script_agrosyst/02_outils/tests/'+'TEST_RESULT_'+ identifiant_test +'.csv')
+
+    res = []
+    for entite_id in list(colonne_to_test_for_ligne.keys()):
+        colonnes_to_test = colonne_to_test_for_ligne[entite_id]
+
+        # valeur trouvée :
+        output = donnees_computed.loc[donnees_computed[key_name] == entite_id]
+        output = output[colonnes_to_test].fillna('').astype('str')
+
+        # valeur attendue :
+        expected_output = df_metadonnees.loc[(df_metadonnees['id_ligne'] == entite_id) & (df_metadonnees['colonne_testee'].isin(colonnes_to_test))]
+        expected_output = expected_output.pivot(columns='colonne_testee', values='valeur_attendue', index='id_ligne').fillna('')
+
+        for colonne_to_test in colonnes_to_test:
+            print(output[colonne_to_test].values)
+            print(expected_output[colonne_to_test].values)
+            print(expected_output[colonne_to_test].values == output[colonne_to_test].values)
+            if(len(expected_output[colonne_to_test].values) > 0):
+                is_null_value_expected = (expected_output[colonne_to_test].values[0] == '')
+
+            if((output[colonne_to_test].values != expected_output[colonne_to_test].values) or (len(output[colonne_to_test].values) == 0 and not is_null_value_expected)):
+                res.append(False)
+            else:
+                res.append(True)
+
+    return res
+
+def test_get_temporal_status_for_each_sdc_dirodur():
+    """
+        Test de l'obtention des informations sur l'etat_temporel des sdc Dirodur
+    """
+
+    identifiant_test = 'test_get_temporal_status_for_each_sdc_dirodur'
+    df_names = [
+        'synthetise', # util + etat_temp
+        'sdc', # util + etat_temp
+        'typologie_assol_can_realise', # util
+        'typologie_can_rotation_synthetise', # util
+        'entite_unique_par_sdc_nettoyage', # util
+        'sdc_realise_performance', # util
+        'synthetise_synthetise_performance', # util
+        'intervention_synthetise_agrege', # util
+        'intervention_realise_agrege', # util
+        'identification_pz0', # etat_temporel
+        'zone', # etat_temporel
+        'parcelle' # etat_temporel
+    ]
+    path_data = '02_outils/tests/data/test_get_temporal_status_for_each_sdc_dirodur/'
+    fonction_to_apply = dirodur_outil.get_temporal_status_for_each_sdc_dirodur
+    res = fonction_test(identifiant_test, df_names, path_data, fonction_to_apply)
+
+    assert all(res)
